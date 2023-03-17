61e83f76f783641ca042275c5e9a919e81733ea8
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-03-17 00:37:17 -0300

    ex010

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade deo seu carro é ${vel}km/h`)
+if (vel > 60){// condiçao simples!, porque nao tem se nao.
+    console.log(`Voce ultrapassou a velocidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
