4c6327369f328e08412b96937f2f28095734bf22
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-03-21 22:20:18 -0300

    Update script.js

--- treinando/script.js
+++ treinando/script.js
@@ -2,8 +2,8 @@ function carregar(){
     var msg =window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 12
+    var hora = data.getHours()
+    //var hora = 12
     msg.innerHTML = `Agora sao ${hora} horas`
     if (hora >= 0 && hora < 12){
         document.body.style.background = '#e2cd2f'
