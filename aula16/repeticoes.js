07cf680398df3a0adcd0509f451746c8336202ea
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-03-23 00:12:18 -0300

    organizando

---
+++ aula16/repeticoes.js
@@ -0,0 +1,15 @@
+/* Estrutura de repeticao comm teste logico no final*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)
+
+/* Estrutura de repeticao com teste logico no inicio
+
+var c = 1
+while (c = 1){
+    console.log(`Passo ${c}`)
+    c++
+} */
